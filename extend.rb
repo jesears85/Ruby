839781161fc86915df7203fb5b14c68826de60f1
Jacob Sears <107888073+jesears85@users.noreply.github.com>
2022-08-05 12:56:38 +0300

    Add files via upload

---
+++ extend.rb
@@ -0,0 +1,16 @@
+module Announcer
+  def who_am_i
+    "The name of this class is #{self}"
+  end
+end
+
+class Dog
+  extend Announcer
+end
+
+class Cat
+  extend Announcer
+end
+
+watson = Dog.new
+p Dog.who_am_i
